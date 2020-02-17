2f92acc0d93ad06dfa37686f6187a5063c127138
MiguelSolisSegura <35377845+MiguelSolisSegura@users.noreply.github.com>
2020-02-17 14:41:29 -0600

    Funciones y Tests.

---
+++ funciones.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def F1(palabra):
+  if type(palabra) != str:
+    return ("ERROR: Tipo de dato incorrecto")
+  else:
+    for letra in palabra:
+      if letra == letra.upper():
+        return ("ERROR: Mayúsculas no admitidas")
+    return palabra.upper()
+
+def F2(palabra):
+  if type(palabra) != str:
+    return ("ERROR: Tipo de dato incorrecto")
+  else:
+    return('w' in palabra) 
+
+def F3(a,b):
+  if type(a) != int or type(b) != int:
+    return("ERROR: Tipo de dato incorrecto")
+  elif a-b<0:
+    return("ERROR: El resultado no pertenece a los números naturales")
+  else:
+    return (a-b)
+
---
+++ tests.py
@@ -0,0 +1,17 @@
+
+from funciones import F1,F2,F3
+
+def test_1():
+
+    assert F1('micros') == 'MICROS'
+    assert F2('while') == True
+    assert F3(5,2) == 3
+    
+def test_2():
+    
+    assert F1(123) == "ERROR: Tipo de dato incorrecto"
+    assert F2(456) == "ERROR: Tipo de dato incorrecto"
+    
+def test_3():
+    
+    assert F3(5,6) == "ERROR: El resultado no pertenece a los números naturales"
