8613f91052be1ad7801aacae403d03fb1e09db29
MiguelSolisSegura <35377845+MiguelSolisSegura@users.noreply.github.com>
2020-02-27 22:43:58 -0600

    Add files via upload

--- tests.py
+++ tests.py
@@ -1,17 +1,20 @@
 
-from funciones import F1,F2,F3
+from funciones import F1, F2, F3
+
 
 def test_1():
 
     assert F1('micros') == 'MICROS'
-    assert F2('while') == True
-    assert F3(5,2) == 3
-    
+    assert F2('while')
+    assert F3(5, 2) == 3
+
+
 def test_2():
-    
+
     assert F1(123) == "ERROR: Tipo de dato incorrecto"
     assert F2(456) == "ERROR: Tipo de dato incorrecto"
-    
+
+
 def test_3():
-    
-    assert F3(5,6) == "ERROR: El resultado no pertenece a los números naturales"
+
+    assert F3(5, 6) == "ERROR: Número no natural"
