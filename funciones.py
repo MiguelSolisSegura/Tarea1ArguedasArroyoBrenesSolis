029fb6565a927ec7b061f8d731ce73ef1f1fbe50
MiguelSolisSegura <35377845+MiguelSolisSegura@users.noreply.github.com>
2020-02-27 22:31:43 -0600

    Add files via upload

--- funciones.py
+++ funciones.py
@@ -1,25 +1,19 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-This is a temporary script file.
-"""
-
-
 def F1(palabra):
+    numeros = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     if type(palabra) != str:
         return ("ERROR: Tipo de dato incorrecto")
     else:
         for letra in palabra:
-            if letra == letra.upper():
+            if letra == letra.upper() and letra not in numeros:
                 return ("ERROR: Mayúsculas no admitidas")
-        return palabra.upper()
+    return palabra.upper()
 
 
 def F2(palabra):
     if type(palabra) != str:
         return ("ERROR: Tipo de dato incorrecto")
     else:
-        return('w' in palabra)
+        return ('w' in palabra)
 
 
 def F3(a, b):
